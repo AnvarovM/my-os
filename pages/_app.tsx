c10a2b695e8d5bb3af51b2b4d99f33f70b551de9
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-01-30 16:00:59 +0500

    added stylelint

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app";
+import { ReactElement } from "react";
+import "styles/globals.scss";
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
