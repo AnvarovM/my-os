8381c51d621b68668c3c6ad67e6daece7ce99312
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-01-30 19:00:06 +0500

    Added rules for .eslint & stylelint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
+import '../styles/globals.scss';
+
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
-import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
