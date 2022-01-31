036ef3aac85b42109eafb20b758a6d58bde5de01
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-01-31 12:00:08 +0500

    Fixed some warnings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.scss';
 
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+// eslint-disable-next-line import/no-unresolved
+import { name } from 'package.json';
 import { ReactElement } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>{ name }</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
