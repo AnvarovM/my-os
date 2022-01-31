df7c1500107a7e6ade692abb351a5aecb6c01685
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-01-31 12:16:32 +0500

    Added meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.scss';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 // eslint-disable-next-line import/no-unresolved
-import { name } from 'package.json';
+import { description, name } from 'package.json';
 import { ReactElement } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
     <>
       <Head>
         <title>{ name }</title>
+        <meta name="description" content={description} />
       </Head>
       <Component {...pageProps} />
     </>
