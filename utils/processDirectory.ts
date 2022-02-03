ca98b79ac6ab30d7984fc97ecd5576b06dc7cc6f
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-03 21:20:39 +0500

    rename jtsx to ts

---
+++ utils/processDirectory.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+import dynamic from 'next/dynamic';
+import type { Processes } from 'types/utils/processDirectory';
+
+const processDirectory: Processes = {
+  HelloWorld: {
+    Component: dynamic(() => import('components/apps/HelloWorld'))
+  }
+};
+
+export default processDirectory;
