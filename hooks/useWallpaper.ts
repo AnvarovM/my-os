0358bee853df17dd9374c62d0c8ee22b91b3c70f
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-09 22:41:57 +0500

    fixed some issues

--- hooks/useWallpaper.ts
+++ hooks/useWallpaper.ts
@@ -10,3 +10,77 @@ const useWallpaper = (desktopRef: React.RefObject<HTMLElement>): void => {
 };
 
 export default useWallpaper;
+
+//  import Color from 'color';
+// import { useEffect } from 'react';
+// import * as THREE from 'three';
+// import type { WallpaperEffect } from 'types/components/System/Desktop/Wallpaper';
+// // import { MILLISECONDS_IN_SECOND } from 'utils/constants';
+// import WAVES from 'vanta/dist/vanta.waves.min';
+
+// const MILLISECONDS_IN_SECOND = 1000;
+
+// const fps = 20;
+// const updateIntervalInMilliseconds = MILLISECONDS_IN_SECOND / fps;
+// const initialColor = 200;
+// const vantaJsSettings = {
+//   gyroControls: false,
+//   mouseControls: false,
+//   touchControls: false,
+//   shininess: 35,
+//   waveHeight: 15,
+//   waveSpeed: 0.25,
+//   zoom: 0.95
+// };
+
+// const initRainbowEffect = (wallpaperEffect: WallpaperEffect): (() => void) => {
+//   let then = Date.now();
+//   let base = initialColor;
+//   let colorUpdateAnimationId: number;
+
+//   const updateColor = () => {
+//     const now = Date.now();
+//     const delta = now - then;
+
+//     if (delta > updateIntervalInMilliseconds) {
+//       base = base > 360 ? 0 : base + 1;
+//       then = now - (delta % updateIntervalInMilliseconds);
+//     }
+
+//     colorUpdateAnimationId = requestAnimationFrame(updateColor);
+//   };
+
+//   colorUpdateAnimationId = requestAnimationFrame(updateColor);
+
+//   return () => {
+//     cancelAnimationFrame(colorUpdateAnimationId);
+//   };
+// };
+
+// const renderWallpaperEffect = ({
+//   current: renderElement
+// }: React.RefObject<HTMLElement>): WallpaperEffect => {
+//   const wallpaperEffect = WAVES({
+//     el: renderElement,
+//     THREE,
+//     ...vantaJsSettings
+//   });
+//   const cancelRainbowEffect = initRainbowEffect(wallpaperEffect);
+
+//   wallpaperEffect.onDestroy = cancelRainbowEffect;
+
+//   return wallpaperEffect;
+// };
+
+// const useWallpaper = (desktopRef: React.RefObject<HTMLElement>): void => {
+//   useEffect(() => {
+//     // eslint-disable-next-line testing-library/render-result-naming-convention
+//     const wallpaperEffect = renderWallpaperEffect(desktopRef);
+
+//     return () => {
+//       wallpaperEffect?.destroy();
+//     };
+//   }, [desktopRef]);
+// };
+
+// export default useWallpaper;
