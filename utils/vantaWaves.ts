adb984602f3df49500c0d52d99c84eaf2ff0d252
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-09 22:57:54 +0500

    Fixed issues

--- utils/vantaWaves.ts
+++ utils/vantaWaves.ts
@@ -8,17 +8,19 @@ const disableControls = {
   toucheControls: false
 };
 
+const isWebLAvailable = typeof WebGLRenderingContext !== 'undefined';
+
 const VantaWaves = (
   settings: VantaWavesSettings
 ): WallpaperEffect => (
   desktopRef: React.RefObject<HTMLElement>
 ) => {
-  const vantaEffect = WAVES({
+  const vantaEffect = isWebLAvailable ? WAVES({
     el: desktopRef.current,
     THREE,
     ...disableControls,
     ...settings
-  });
+  }) : undefined;
 
   return () => {
     vantaEffect?.destroy?.();
