de16f8a2cb7196c21961f66da399d13b2f12d8e3
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-10 16:04:34 +0500

    Fixed/Refactored to best practices

--- _test_/pages/index.spec.tsx
+++ _test_/pages/index.spec.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable import/no-unresolved */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import StyledApp from 'components/pages/StyledApps';
 // eslint-disable-next-line import/no-unresolved
 import Index from 'pages/index';
 
-test('renders index page', () => {
-  const { getByText } = render(
+test('renders main role', () => {
+  render(
     <StyledApp>
       <Index />
     </StyledApp>
   );
   // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(getByText('Hi how are you?')).toBeInTheDocument();
+  expect(screen.getByRole('main')).toBeInTheDocument();
 });
