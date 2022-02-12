7aecea1540edf784f77d15cdc7c4ebea705ed7a1
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-12 08:10:46 +0500

    fxed npm run build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import Desktop from 'components/system/Desktop';
 import ProcessorLoader from 'components/system/Processes/ProcessorLoader';
-import { FileSystemConsumer } from 'contexts/fileSystem';
 import { ProcessProvider } from 'contexts/process';
 import type { FC } from 'react';
 import { getStartupProcesses } from 'utils/processDirectory';
@@ -9,11 +8,6 @@ import { getStartupProcesses } from 'utils/processDirectory';
 const Home: FC = () => {
   return (
     <Desktop>
-      <FileSystemConsumer>
-        {({ fs }) => {
-          console.log(fs);
-        }}
-      </FileSystemConsumer>
       <ProcessProvider startupProcesses={getStartupProcesses()}>
         <ProcessorLoader />
       </ProcessProvider>
