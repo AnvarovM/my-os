42931cd1c611ce3f747eb618517004df94fc489e
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-02-28 20:05:53 +0500

    added min and max process functions

--- utils/processFunctions.ts
+++ utils/processFunctions.ts
@@ -3,10 +3,30 @@ import { processDirectory } from 'utils/processDirectory';
 
 export const closeProcess =
   (pid: string) =>
-  ({ [pid]: _closeProcess, ...remainingProcesses }: Processes) =>
+  ({ [pid]: _closeProcess, ...remainingProcesses }: Processes): Processes =>
     remainingProcesses;
 
 export const openProcess = (pid: string) => (currentProcesses: Processes) =>
   currentProcesses[pid] || !processDirectory[pid]
     ? currentProcesses
     : { ...currentProcesses, [pid]: processDirectory[pid] };
+
+export const toggleProcessSetting =
+  (pid: string, setting: 'maximize' | 'minimize') =>
+  ({ [pid]: process, ...otherProcesses }: Processes): Processes =>
+    process
+      ? {
+          [pid]: { ...process, [setting]: !process[setting] },
+          ...otherProcesses
+        }
+      : otherProcesses;
+
+export const maximizeProcess =
+  (pid: string) =>
+  (processes: Processes): Processes =>
+    toggleProcessSetting(pid, 'maximize')(processes);
+
+export const maximizeProcess =
+  (pid: string) =>
+  (processes: Processes): Processes =>
+    toggleProcessSetting(pid, 'maximize')(processes);
