ee61f06349b27d9fe621fbd058ec550415614700
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-02 13:22:49 +0500

    Extracted defaul window constants && o'zgarchilar qatoriga qo'shildi

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,16 @@
-/* eslint-disable import/prefer-default-export */
+import type { Size } from 'hooks/useDraggableAndResizable';
+import type { Position } from 'react-rnd';
+
+export const DEFAULT_WINDOW_SIZE: Size = {
+  height: '200px',
+  width: '300px'
+};
+
+export const DEFAULT_WINDOW_POSITION: Position = {
+  x: 0,
+  y: 0
+};
+
 export const MILLISECONDS_IN_SECONDS = 1000;
 
 export const IMAGE_FILE_EXTENSION = [
