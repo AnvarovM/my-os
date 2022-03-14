44636df0bd87905587f266bf74c5e2582352a0de
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-14 08:05:47 +0500

    Tweaks to taskba entries

--- styles/components/system/Taskbar/StyledTaskbarEntries.ts
+++ styles/components/system/Taskbar/StyledTaskbarEntries.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const StyledStartButton = styled.ul`
+  column-gap: 1px;
   display: flex;
   height: 100%;
   left: ${({ theme }) => theme.sizes.StartButton.width};
+  margin: 0 3px;
   position: absolute;
   right: ${({ theme }) => theme.sizes.clock.width};
 `;
