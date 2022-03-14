175ff3ebe95b3d10a0cc7649ae2197284f55cdeb
Anvarovm <anvarovmuxammadxuja2001@gmail.com>
2022-03-14 21:04:57 +0500

    Save session to file

--- contexts/session/useSessionContextState.tsx
+++ contexts/session/useSessionContextState.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-unresolved */
+import { useFileSystem } from 'contexts/fileSystem';
 import type { Size } from 'hooks/useRnd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { Position } from 'react-rnd';
+
+const SESSION_FILE = '/session.json';
 
 type WindowState = {
   position?: Position;
@@ -9,26 +12,50 @@ type WindowState = {
 };
 
 type WindowStates = {
-  [pid: string]: WindowState;
+  [id: string]: WindowState;
 };
 
 export type SessionContextState = {
   setThemeName: React.Dispatch<React.SetStateAction<string>>;
-  setWindowState: React.Dispatch<React.SetStateAction<WindowStates>>;
+  setWindowStates: React.Dispatch<React.SetStateAction<WindowStates>>;
   themeName: string;
   windowStates: WindowStates;
 };
 
 const useSessionContextState = (): SessionContextState => {
+  const { fs } = useFileSystem();
+  const [sessionLoaded, setSessionLoaded] = useState(false);
   const [themeName, setThemeName] = useState('');
-  const [windowStates, setWindowState] = useState<WindowStates>({});
+  const [windowStates, setWindowStates] = useState<WindowStates>({});
 
-  return {
-    themeName,
-    setThemeName,
-    setWindowState,
-    windowStates
-  };
+  useEffect(() => {
+    if (sessionLoaded) {
+      fs?.writeFile(
+        SESSION_FILE,
+        JSON.stringify({
+          themeName,
+          windowStates
+        })
+      );
+    }
+  }, [fs, sessionLoaded, themeName, windowStates]);
+
+  useEffect(
+    () =>
+      fs?.readFile(SESSION_FILE, (_error, contents) => {
+        if (contents) {
+          const session = JSON.parse(contents.toString());
+
+          setThemeName(session.themeName);
+          setWindowStates(session.windowStates);
+        }
+
+        setSessionLoaded(true);
+      }),
+    [fs]
+  );
+
+  return { setThemeName, setWindowStates, themeName, windowStates };
 };
 
 export default useSessionContextState;
